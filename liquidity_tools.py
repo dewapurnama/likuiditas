d8aba096eecb2529c8410011478088ed33e528b1
dewapurnama <75442383+dewapurnama@users.noreply.github.com>
2025-07-21 11:10:49 +0700

    Update liquidity_tools.py

--- liquidity_tools.py
+++ liquidity_tools.py
@@ -14,7 +14,7 @@ tab0.markdown(
 with tab0:
     col1, col2, col3, col4 = st.columns(4)
     with col1:
-        st.metric("🔥 Likuiditas Wajib", "2.02x BPIH", delta="25.47% YoY / -1.00% MoM", delta_color="off", border=True, help="Angka di atas bulan sekarang bersifat proyeksi", label_visibility="visible")
+        st.metric("🔥 Likuiditas Wajib", "2.02x BPIH", "25.47% YoY, -1.00% MoM", border=True, help="Angka di atas bulan sekarang bersifat proyeksi", label_visibility="visible")
     with col2:
         st.metric("📊 Investasi Jangka Pendek", "8,44 triliun", "", border=True, help="Angka di atas bulan sekarang bersifat proyeksi", label_visibility="visible")
     with col3:
